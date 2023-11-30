1e25fdfe3cad76767f388e52355076833f13dc20
Marllisson <mar.hellsing@gmal.com>
2023-11-30 10:23:13 -0300

    Conclusão ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner sc = new Scanner(System.in);
+       int numero;
+       String agencia;
+       String nome;
+       double saldo;
+
+       System.out.println("Informe o Numero da conta: ");
+       numero = sc.nextInt(); 
+
+       sc.nextLine(); 
+       System.out.println("Informe Seu Agencia: ");
+       agencia = sc.nextLine();
+
+       System.out.println("Informe Seu Nome: ");
+       nome = sc.nextLine();
+
+       System.out.println("Informe Seu Saldo ");
+       saldo = sc.nextDouble();
+
+       System.out.println("Olá "+ nome +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque"); 
+
+
+
+        
+
+
     }
 }
