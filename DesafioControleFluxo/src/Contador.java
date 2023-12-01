4aad8394b4920a2add17ae49a33a954afa1aa912
Marllisson <mar.hellsing@gmal.com>
2023-12-01 01:33:17 -0300

    Criando Desafio de Fluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = sc.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = sc.nextInt();
+
+        sc.close();
+
+
+
+
+
+    }
+
+
+    
+}
